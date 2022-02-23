9c7ae84d06ae5dbb896b9aae197b58069ec11018
Wissam Youssef <wissam@users.noreply.github.com>
2022-02-23 18:25:14 -0500

    just a test push

--- go.mod
+++ go.mod
@@ -12,7 +12,29 @@ require (
 require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/hofstadter-io/hof v0.6.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/magiconair/properties v1.8.1 // indirect
+	github.com/mattn/go-colorable v0.1.6 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/mattn/go-sqlite3 v1.14.9 // indirect
+	github.com/mitchellh/mapstructure v1.3.2 // indirect
+	github.com/pelletier/go-toml v1.8.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/spf13/afero v1.2.2 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.7.0 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	github.com/wissam/beacon v0.0.0-20220211003556-1c96fc269ef3 // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+	golang.org/x/text v0.3.6 // indirect
+	gopkg.in/inconshreveable/log15.v2 v2.0.0-20200109203555-b30bc20e4fd1 // indirect
+	gopkg.in/ini.v1 v1.57.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -1,96 +1,21 @@
 package bot
 
 import (
-	"context"
 	"log"
 	"os"
-	"strings"
 
 	"github.com/gempir/go-twitch-irc/v2"
-	"github.com/go-redis/redis/v8"
-	"github.com/wissam/gerald/internal/commands"
-	"github.com/wissam/gerald/internal/db"
 	"github.com/wissam/gerald/internal/models"
-	"github.com/wissam/gerald/internal/tapi"
+	"github.com/wissam/gerald/internal/vislog" //I am very confused...
 )
 
-//********************************************************************************************************
-// Migrate all DB stuff to db module
-// Migrate all irc stuff to irc module
-// Migrate all redis stuff to redis module
-//temporary struct channel until I figure out the db
-// I should probably have models first... then build on top bi by bit to tame
-// the chaos...
-var dbi db.DB
 var client *twitch.Client
 
-//yep good enough for now...
-// is checking services stupid? what is best practice?
-func CheckServices() {
-	log.Println("Checking Services")
-	//need to pretify this? googling...
-}
-
-//func Check
 func Run() {
-	dbi = db.DB{}
-	dbi.Connect()
-	dbi.Migrate()
-	//temporary redis connect , and sub
-	// DO NOT KEEP!!!!
-	// **********************************************************************
-	// Testing connection failure!
-	var ctx = context.Background()
-	//rmsg := make(chan redis.Message)
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
-	newchan := make(chan string)
-	go func() {
-		subscriber := rdb.Subscribe(ctx, "one")
-		for {
-			log.Println("looping")
-			rmsg := subscriber.Channel()
-			select {
-			case rchan, ok := <-rmsg:
-				if ok {
-					log.Printf("Got a redis payload with %s\n", rchan.Payload)
-					newchan <- rchan.Payload
-					log.Println("unblocked payload and went to newchan")
-				}
-			}
-		}
-	}()
-
-	channels := []models.Channel{{Name: "MadMistro", ID: "38429111"}, {Name: "Kodder", ID: "101185038"}}
-	for i := 0; i < len(channels); i++ {
-		channels[i].Emotes = tapi.GetAllEmotes(channels[i].ID)
-		log.Printf("len inside first loop %d\n", len(channels[i].Emotes))
-	}
-	for _, channel := range channels {
-
-		log.Printf("len second loop %d\n", len(channel.Emotes))
-		for _, emote := range channel.Emotes {
-
-			log.Println(emote)
-		}
-	}
-	// lifewithbugs is a hero!
-	// Learn the proper convention for "listen loop" or "waiting loop"
-	go func() {
-		for {
-			AddChannel(newchan)
-		}
-	}()
+	channels := []models.Channel{{Name: "Kodder", ID: "101185038"}}
 	client = twitch.NewClient(os.Getenv("NICKNAME"), os.Getenv("OAUTH"))
 	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
-		if strings.HasPrefix(message.Message, "!") {
-			CommandsParser(message)
-		} else {
-			MessageParser(message)
-		}
+		NickBulbColour(message.User.Color)
 	})
 	for _, channel := range channels {
 		client.Join(channel.Name)
@@ -104,39 +29,7 @@ func Run() {
 	}
 }
 
-//I am not sure how design this...
-func AddChannel(nchan chan string) {
-	log.Println("pre channel assignment to see if this is getting blocked")
-	//this needs a select with a cases. including "done".
-	newchan := <-nchan
-	log.Printf("Attempting to join %s\n", newchan)
-	client.Join(newchan)
-}
-func CommandsParser(message twitch.PrivateMessage) {
-
-	m := map[string]func(twitch.PrivateMessage, db.DB) string{
-		"hi":        commands.Hi,
-		"hello":     commands.Hello,
-		"favourite": commands.Favourite,
-		"emotes":    commands.Emotes,
-	}
-	firstword := strings.Fields(message.Message)[0][1:]
-	if val, exists := m[firstword]; exists {
-		client.Say(message.Channel, val(message, dbi))
-	}
-}
-
-//I need to add emotes only related to the room/channel that is currently being
-//used , or you would have to give the bot subscription in every channel where
-//any emote has been used ever... gosh that's a lot of money for nothing...
-// So design change, maybe I should only have a counter with the emotes only?
-// Let's see how to get the ownership of emote before thinking about anything
-// else...
-func MessageParser(message twitch.PrivateMessage) {
-	if message.Emotes != nil {
-		for _, e := range message.Emotes {
-			dbi.EmoteCountInsert(message.User.ID, message.RoomID, e.ID, e.Count)
-		}
-	}
-	log.Println(message.Channel) //this is magic, it solves all problems on earth...
+func NickBulbColour(color string) {
+	blb := vislog.NewBulb("d073d567639b")
+	blb.HEX(color)
 }
